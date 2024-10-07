f61f7499272e3dd5c01fe85582c4c19b0d92fa8f
Khoa Truong <truongndkgch190486@fpt.edu.vn>
2024-10-07 10:07:44 +0700

    ok

---
+++ src/routes/api.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const routerAPI = express.Router();
+
+// const { getUsersAPI, postCreateUserAPI,
+//     putUpdateUserAPI, deleteUserAPI
+
+// } = require('../controllers/apiController')
+
+
+// routerAPI.get('/users', getUsersAPI);
+// routerAPI.post('/users', postCreateUserAPI);
+// routerAPI.put('/users', putUpdateUserAPI);
+// routerAPI.delete('/users', deleteUserAPI);
+
+routerAPI.get("/", (req, res) => {
+    return res.status(200).json( {message: "Welcome to API"})
+})
+
+
+module.exports = routerAPI; //export default
